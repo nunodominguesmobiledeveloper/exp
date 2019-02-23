4016e8d71c394a7435a3241f867e29ca8baf482c
Nuno Domingues <nunogdomingues@gmail.com>
2019-02-23 11:31:21 +0000

    expmanager

---
+++ optionals/optionals/Expmanager.swift
@@ -0,0 +1,13 @@
+//
+//  Expmanager.swift
+//  optionals
+//
+//  Created by Nuno Domingues on 23/02/2019.
+//  Copyright © 2019 Nuno Domingues. All rights reserved.
+//
+
+import Foundation
+
+struct ExpManager {
+    let experience: Exp?
+}
